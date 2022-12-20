210bace3d380c4822f3b49b70cb614c3dd09db75
Elin <365433079@qq.com>
2022-12-20 15:49:17 +0800

    修改

--- source/conf.py
+++ source/conf.py
@@ -20,8 +20,8 @@
 # -- Project information -----------------------------------------------------
 
 project = u'ReStructuredText Tutorial'
-copyright = u'2019, 王竹兴'
-author = u'王竹兴'
+copyright = u'2019, Elinpf'
+author = u'Elinpf'
 
 # The short X.Y version
 version = u''
